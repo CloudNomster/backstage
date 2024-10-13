f1ab199a9be2e708a26d88392b4244bb997927f9
Markku Kokko <markku@techmonkeys.se>
2024-10-13 22:34:45 +0200

    Added entity provider for entra

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -24,6 +24,7 @@ backend.add(import('@backstage/plugin-catalog-backend/alpha'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-msgraph/alpha'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
